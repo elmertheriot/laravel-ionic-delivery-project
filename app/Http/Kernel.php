f703acae34a662abd80440a47bb0e9aa74c4a23e
elmertheriot <elmertheriot@gmail.com>
2016-05-25 14:15:47 -0300

    first commit

---
+++ app/Http/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DOLucasDelivery\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \DOLucasDelivery\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \DOLucasDelivery\Http\Middleware\VerifyCsrfToken::class,
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \DOLucasDelivery\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \DOLucasDelivery\Http\Middleware\RedirectIfAuthenticated::class,
+    ];
+}
