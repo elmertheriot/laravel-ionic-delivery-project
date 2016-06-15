ffbd38c9f73bc41086deb9957c494cc140ffe9d1
elmertheriot <elmertheriot@gmail.com>
2016-06-14 22:17:48 -0300

    update repositoryServiceProvider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -18,5 +18,25 @@ class RepositoryServiceProvider extends ServiceProvider
             'DOLucasDelivery\Repositories\CategoryRepository',
             'DOLucasDelivery\Repositories\CategoryRepositoryEloquent'
         );
+        $this->app->bind(
+            'DOLucasDelivery\Repositories\ClientRepository',
+            'DOLucasDelivery\Repositories\ClientRepositoryEloquent'
+        );
+        $this->app->bind(
+            'DOLucasDelivery\Repositories\OrderItemRepository',
+            'DOLucasDelivery\Repositories\OrderItemRepositoryEloquent'
+        );
+        $this->app->bind(
+            'DOLucasDelivery\Repositories\OrderRepository',
+            'DOLucasDelivery\Repositories\OrderRepositoryEloquent'
+        );
+        $this->app->bind(
+            'DOLucasDelivery\Repositories\ProductRepository',
+            'DOLucasDelivery\Repositories\ProductRepositoryEloquent'
+        );
+        $this->app->bind(
+            'DOLucasDelivery\Repositories\UserRepository',
+            'DOLucasDelivery\Repositories\UserRepositoryEloquent'
+        );
     }
 }
