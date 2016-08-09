9820e7ea93ab3b76ff2acf013eec3b078981bf98
elmertheriot <elmertheriot@gmail.com>
2016-08-08 22:25:19 -0300

    indent

--- app/Repositories/UserRepositoryEloquent.php
+++ app/Repositories/UserRepositoryEloquent.php
@@ -14,6 +14,7 @@ use DOLucasDelivery\Validators\UserValidator;
  */
 class UserRepositoryEloquent extends BaseRepository implements UserRepository
 {
+    
     /**
      * Specify Model class name
      *
