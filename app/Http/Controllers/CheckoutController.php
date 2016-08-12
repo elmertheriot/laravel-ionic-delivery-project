7b0f272f0a8439f08a78e04997e19aa89b5dd0d8
elmertheriot <elmertheriot@gmail.com>
2016-08-11 22:02:35 -0300

    Implementando CheckoutController

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -9,6 +9,7 @@ use DOLucasDelivery\Http\Controllers\Controller;
 use DOLucasDelivery\Repositories\OrderRepository;
 use DOLucasDelivery\Repositories\UserRepository;
 use DOLucasDelivery\Repositories\ProductRepository;
+use DOLucasDelivery\Services\OrderService;
 
 class CheckoutController extends Controller
 {
@@ -28,14 +29,32 @@ class CheckoutController extends Controller
      */
     private $productRepository;
 
+    /**
+     * @var OrderService
+     */
+    private $orderService;
+
     public function __construct(
         OrderRepository $orderRepository,
         UserRepository $userRepository,
-        ProductRepository $productRepository
+        ProductRepository $productRepository,
+        OrderService $orderService
     ) {
         $this->orderRepository   = $orderRepository;
         $this->userRepository    = $userRepository;
         $this->productRepository = $productRepository;
+        $this->orderService      = $orderService;
+    }
+
+    public function index()
+    {
+        $clientId = $this->userRepository->find(Auth::user()->id)->client->id;
+
+        $orders = $this->orderRepository->scopeQuery(function ($query) use ($clientId) {
+            return $query->where('client_id', '=', $clientId);
+        })->paginate();
+
+        return view('customer.order.index', compact('orders'));
     }
 
     public function create()
@@ -45,8 +64,14 @@ class CheckoutController extends Controller
         return view('customer.order.create', compact('products'));
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        
+        $data = $request->all();
+        $clientId = $this->userRepository->find(Auth::user()->id)->client->id;
+        $data['client_id'] = $clientId;
+
+        $this->orderService->create($data);
+
+        return redirect()->route('customer.order.index');
     }
 }
