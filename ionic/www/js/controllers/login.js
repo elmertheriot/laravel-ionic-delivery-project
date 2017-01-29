1538cb40f09178aadcb8d7000f9d589454c4bf8a
elmertheriot <elmertheriot@gmail.com>
2017-01-29 00:46:18 -0200

    add ionic

---
+++ ionic/www/js/controllers/login.js
@@ -0,0 +1,39 @@
+angular
+	.module('delivery.controllers', [])
+	.controller('LoginController', LoginController);
+
+LoginController.$inject = [
+	'$scope',
+	'OAuth',
+	'$ionicPopup',
+	'$state'
+];
+
+function LoginController(
+	$scope, 
+	OAuth, 
+	$ionicPopup,
+	$state
+) {
+	$scope.user = {
+		username: '',
+		password: ''
+	};
+	
+	$scope.login = login;
+	
+	function login() {
+		OAuth.getAccessToken($scope.user).then(success, error);
+		
+		function success(res) {
+			$state.go('home');
+		}
+		
+		function error(err) {
+			$ionicPopup.alert({
+				title: 'Warning',
+				template: 'Invalid username or password'
+			});
+		}
+	}
+}
